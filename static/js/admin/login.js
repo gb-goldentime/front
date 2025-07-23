dfe874e53b78a3e2bddd42415e249bbe31abe06c
dev.temin018 <dev.gtaemin254@gmail.com>
2025-07-24 00:43:07 +0900

    관리자로그인

--- static/js/admin/login.js
+++ static/js/admin/login.js
@@ -1,1 +1,6 @@
-const 
+const check = document.querySelectorAll(".boot-check-box");
+check.forEach((box) => {
+    box.addEventListener("click", () => {
+        box.classList.toggle("checked");
+    });
+});
