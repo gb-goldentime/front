70e7686c45e9d69aefc07352fed9600ab2d8f8d2
dev.temin018 <dev.gtaemin254@gmail.com>
2025-07-24 19:03:51 +0900

    관리자페이지로그인 #1

--- static/js/admin/login.js
+++ static/js/admin/login.js
@@ -1,9 +1,17 @@
 const checkbox = document.querySelector("input[type=checkbox]");
-const autoCheckbox = document.getElementById("auto-checkbox");
+const autoCheckbox = document.getElementById("autoCheckbox");
+const checkIcon = document.getElementById("check-icon");
 
-checkbox.addEventListener("click", (e) => {
-    if(e.target.checked){
-        const value = e.target.value
-        auto-checkbox = value ? 
-    }
+checkbox.addEventListener("change", (e) => {
+    console.log(e.target.checked);
+    checkIcon.classList.toggle("active", e.target.checked);
+});
+
+const loginbutton = document.getElementById("login-button");
+const emailinput = document.getElementById("email-input");
+const passwordinput = document.getElementById("password-input");
+const emailmodal = document.getElementById("email-test");
+const passwordmodal = document.getElementById("password-test");
+loginbutton.addEventListener("click", (e) => {
+    emailmodal.style.display = "inline";
 });
