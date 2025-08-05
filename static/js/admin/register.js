f3e4df131e5f16e2caa203b27b222ca038407af0
dev.temin018 <dev.gtaemin254@gmail.com>
2025-08-05 20:31:44 +0900

    관리자페이지가입 #90

--- static/js/admin/register.js
+++ static/js/admin/register.js
@@ -14,3 +14,52 @@ menuBtns.forEach((btn) => {
         if (targetMenu) targetMenu.style.display = "block";
     });
 });
+
+// 일반회원 상세 모달 창 열고 닫는 이벤트
+const modal = document.querySelector(".member-modal");
+const actionButtons = document.querySelectorAll(".action-btn");
+const closeButtons = document.querySelectorAll(".close");
+const closeFooterButton = document.querySelector(".btn-close");
+
+actionButtons.forEach((actionButton) => {
+    actionButton.addEventListener("click", (e) => {
+        modal.style.display = "block";
+
+        setTimeout(() => {
+            modal.classList.add("show");
+            modal.style.background = "rgba(0,0,0,0.5)";
+            document.body.classList.add("modal-open");
+        }, 100);
+    });
+});
+
+closeButtons.forEach((closeButton) => {
+    closeButton.addEventListener("click", (e) => {
+        modal.classList.remove("show");
+        document.body.classList.remove("modal-open");
+
+        setTimeout(() => {
+            modal.style.display = "none";
+        }, 100);
+    });
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.classList.remove("show");
+        document.body.classList.remove("modal-open");
+
+        setTimeout(() => {
+            modal.style.display = "none";
+        }, 100);
+    }
+});
+
+closeFooterButton.addEventListener("click", (e) => {
+    modal.classList.remove("show");
+    document.body.classList.remove("modal-open");
+
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 100);
+});
