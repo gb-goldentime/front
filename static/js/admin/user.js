16f7328239916c2ff945e8002402e0751398015a
dev.temin018 <dev.gtaemin254@gmail.com>
2025-08-06 12:23:52 +0900

    관리자페이지일반회원목록 #98

--- static/js/admin/user.js
+++ static/js/admin/user.js
@@ -1,3 +1,22 @@
+// 사이드바 메뉴 토글
+const menuBtns = document.querySelectorAll(".menu-btn");
+const allSubMenus = document.querySelectorAll(".menu-sub-list");
+
+menuBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        allSubMenus.forEach((submenu) => (submenu.style.display = "none"));
+        menuBtns.forEach((b) => b.classList.remove("active"));
+
+        this.classList.add("active");
+
+        const targetId = this.getAttribute("aria-controls");
+        const targetMenu = document.getElementById(targetId);
+        if (targetMenu) targetMenu.style.display = "block";
+    });
+});
+
+//
+
 const homeButton = document.getElementById("menu-home");
 const sideMenuButtons = document.querySelectorAll(".menu-btn");
 const sideSubLists = document.querySelectorAll(".menu-sub-list");
@@ -5,101 +24,18 @@ const sideSubLinks = document.querySelectorAll(".rebound-link");
 const tabNames = document.querySelectorAll(".tab-name");
 const icons = document.querySelectorAll(".icon-wrapper i");
 
-// 홈 클릭 이벤트
-homeButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    sideMenuButtons.forEach((homeByButton) => {
-        homeByButton.classList.remove("current");
-    });
-    homeButton.classList.add("current");
-
-    sideSubLists.forEach((homeByList) => {
-        homeByList.classList.remove("show");
-    });
-
-    sideSubLinks.forEach((homeByLink) => {
-        homeByLink.classList.remove("active");
-    });
-
-    icons.forEach((homeByIcon) => {
-        homeByIcon.classList.remove("mdi-chevron-down");
-        homeByIcon.classList.add("mdi-chevron-right");
-    });
-});
-
 // 사이드 바 메인 메뉴 클릭 시 리스트 열고 닫기 + 아이콘
 // 사이드 바 서브 링크 클릭 시 이벤트 + 다른 리스트 닫기
 // 상단 tab바 이벤트
-sideMenuButtons.forEach((sideMenuButton) => {
-    sideMenuButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        //기존(a 태그)의 이벤트를 멈추고 js로 직접 처리하기 위해 사용
 
-        const targetId = sideMenuButton.getAttribute("aria-controls");
-        // aria-controls와 같은 속성 가져올 땐 getAttribute();
-        const targetSubList = document.getElementById(targetId);
-        const targetIcon = sideMenuButton.querySelector(".icon-wrapper i");
-
-        if (targetSubList.classList.contains("show")) {
-            targetSubList.classList.remove("show");
-            targetIcon.classList.remove("mdi-chevron-down");
-            targetIcon.classList.add("mdi-chevron-right");
-        } else {
-            targetSubList.classList.add("show");
-            targetIcon.classList.remove("mdi-chevron-right");
-            targetIcon.classList.add("mdi-chevron-down");
-        }
-    });
-});
-
-sideSubLinks.forEach((sideSubLink) => {
-    sideSubLink.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        // 모든 서브 링크 active 초기화
-        sideSubLinks.forEach((resetSubLink) => {
-            resetSubLink.classList.remove("active");
-        });
-        sideSubLink.classList.add("active");
-
-        // 상위 메뉴 버튼 current 초기화 + 설정
-        sideMenuButtons.forEach((resetMenuButton) => {
-            resetMenuButton.classList.remove("current");
-        });
-
-        const parentSubList = sideSubLink.parentElement.parentElement;
-        const parentButton = document.querySelector(
-            `.menu-btn[aria-controls = "${parentSubList.id}"]`
-        );
-        parentButton.classList.add("current");
-
-        // 다른 리스트 닫기
-        sideSubLists.forEach((sideSubList) => {
-            if (sideSubList !== parentSubList) {
-                sideSubList.classList.remove("show");
-
-                const nonTargetButton = document.querySelector(
-                    `.menu-btn[aria-controls="${sideSubList.id}"]`
-                );
-                if (nonTargetButton) {
-                    const nonTargetIcon =
-                        nonTargetButton.querySelector(".icon-wrapper i");
-                    nonTargetIcon.classList.remove("mdi-chevron-down");
-                    nonTargetIcon.classList.add("mdi-chevron-right");
-                }
-            }
-        });
-
-        // 상단 tab-name 활성화
-        const linkText = sideSubLink.textContent.trim().replace("-", "").trim();
-        // 링크 이름에서 기호(-)랑 공백 제거한 텍스트
-        tabNames.forEach((tabName) => tabName.classList.remove("active"));
-        tabNames.forEach((tabNameCheck) => {
-            if (tabNameCheck.textContent.trim() === linkText) {
-                tabNameCheck.classList.add("active");
-            }
-        });
-    });
+// 상단 tab-name 활성화
+const linkText = sideSubLink.textContent.trim().replace("-", "").trim();
+// 링크 이름에서 기호(-)랑 공백 제거한 텍스트
+tabNames.forEach((tabName) => tabName.classList.remove("active"));
+tabNames.forEach((tabNameCheck) => {
+    if (tabNameCheck.textContent.trim() === linkText) {
+        tabNameCheck.classList.add("active");
+    }
 });
 
 // 상단 tab-name 누르면 사이드 바 따라가는 이벤트
@@ -138,7 +74,6 @@ tabNames.forEach((headerTabname) => {
     });
 });
 
-// 상단 오른쪽 관리자 이메일 클릭 시 리스트 출력
 // 출력된 리스트 다시 닫기
 const userMenuWrapper = document.querySelector(".user-menu-wrapper");
 const userMenuContent = document.querySelector(".user-menu-content");
