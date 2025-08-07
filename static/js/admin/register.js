ed0c06f0174090ce55969ae47b89ecd2d32f9da4
dev.temin018 <dev.gtaemin254@gmail.com>
2025-08-07 15:32:22 +0900

    관리자페이지가입수정

--- static/js/admin/register.js
+++ static/js/admin/register.js
@@ -138,3 +138,20 @@ refuseBtn.forEach((btn) => {
         status.style.color = "red";
     });
 });
+
+// 사이드바 메뉴 토글
+const menuBtns = document.querySelectorAll(".menu-btn");
+const allSubMenus = document.querySelectorAll(".menu-sub-list");
+
+menuBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        allSubMenus.forEach((submenu) => (submenu.style.display = "none"));
+        menuBtns.forEach((b) => b.classList.remove("active"));
+
+        this.classList.add("active");
+
+        const targetId = this.getAttribute("aria-controls");
+        const targetMenu = document.getElementById(targetId);
+        if (targetMenu) targetMenu.style.display = "block";
+    });
+});
